a113a5f44f4503937b5fbe1d3baa6bc735ce6a32
tserendulamB241910025 <tserendulamtuvshinjargal85@gmail.com>
2025-05-14 23:43:37 +0800

    Update ZooApp.java

--- src/ZooApp.java
+++ src/ZooApp.java
@@ -3,14 +3,20 @@ import java.util.List;
 
 public class ZooApp {
 	public static void main(String[] args) {
-		Zoo<Mammal> mammalZoo = new Zoo<>();
-		mammalZoo.addAnimal(new Mammal("Арслан"));
-		Zoo<Bird> birdZoo = new Zoo<>();
-		birdZoo.addAnimal(new Bird("Тогоруу"));
-		List<Animal> generalZoo = new ArrayList<>();
-		mammalZoo.transferAnimals(mammalZoo.getAnimals(), generalZoo);
-		birdZoo.transferAnimals(birdZoo.getAnimals(), generalZoo);
-		generalZoo.forEach(a -> System.out.println(a.getName()));
-	}
+		List<Mammal> mammals = new ArrayList<>();
+		mammals.add(new Mammal("Arslan"));
 
+		List<Animal> animals = new ArrayList<>();
+		List<Bird> birds = new ArrayList<>();
+		birds.add(new Bird("Togoruu"));
+
+		Zoo<Mammal> mammalZoo = new Zoo<>();
+		mammalZoo.transferAnimals(mammals, animals);
+		Zoo<Bird> birdZoo = new Zoo<>();
+		birdZoo.transferAnimals(birds, animals); 
+
+		for (Animal animal : animals) {
+			System.out.println(animal.getName());
+		}
+	}
 }
